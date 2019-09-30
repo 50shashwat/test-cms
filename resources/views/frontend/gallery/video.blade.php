98e3d42a02a4011cd1685d022059b0c6c8259f7c
Shashwat Singh <parihar.shashwat@gmail.com>
2019-09-30 19:27:50 +0530

    video page added

--- resources/views/frontend/gallery/video.blade.php
+++ resources/views/frontend/gallery/video.blade.php
@@ -1,7 +1,45 @@
 @extends('layouts.frontend.main')
 
 @section('content')
-    <div class="container" style="text-align:center;padding:40px">
-        <h2>Will Be Updated Soon</h2>
+<main>
+
+<!-- pageBdWrapNav -->
+<nav class="pageBdWrapNav bg-light" aria-label="breadcrumb">
+    <div class="container">
+        <div class="row align-items-md-center">
+            <div class="col-12 col-md-7">
+                <!-- breadcrumb -->
+                <ol class="breadcrumb pageBreadcrumb m-0 p-0 text-capitalize">
+                    <li class="breadcrumb-item">
+                        <a href="home.html">
+                            <i class="fas fa-home icn"><span class="sr-only">icon</span></i>
+                            Home
+                        </a>
+                    </li>
+                    <li class="breadcrumb-item active" aria-current="page">Video Gallery</li>
+                </ol>
+            </div>
+            <div class="col-12 col-md-5 d-none d-md-flex align-items-md-center justify-content-md-end">
+                <!-- title -->
+                <strong class="title d-block text-right fontRoboto fwMedium text-capitalize text-dark">Video Gallery</strong>
+            </div>
+        </div>
     </div>
+</nav>
+<!-- contactAreaBlock -->
+<div class="container">
+    <div class="row" style="margin: 70px auto">
+        <div class="col-md-6" style="text-align:center">
+            
+            <video width="320" height="240" controls>
+                <source src="/downloads/video_1.mp4" type="video/mp4">
+                Your browser does not support the video playback.
+            </video>     
+        </div>
+    
+    </div>
+</div>
+
+</main>
+
 @endsection
