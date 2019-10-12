542db686266b7b8e4ad295e79d18245ffec99e2b
Shashwat Singh <parihar.shashwat@gmail.com>
2019-10-12 12:17:56 +0530

    added achievements page

--- resources/views/frontend/static/displaydisign.blade.php
+++ resources/views/frontend/static/displaydisign.blade.php
@@ -1,10 +1,43 @@
 @extends('layouts.frontend.main')
 
 @section('content')
-<div class="container" style="text-align:center;padding:40px">
-    <video width="320" height="240" controls style="width:100%;height:auto">
-        <source src="{{ asset('videos/dnd.mp4') }}" type="video/mp4">
-        Your browser does not support the video playback.
-    </video>    
-    </div>
+<main>
+
+    <!-- pageBdWrapNav -->
+    <nav class="pageBdWrapNav bg-light" aria-label="breadcrumb">
+        <div class="container">
+            <div class="row align-items-md-center">
+                <div class="col-12 col-md-7">
+                    <!-- breadcrumb -->
+                    <ol class="breadcrumb pageBreadcrumb m-0 p-0 text-capitalize">
+                        <li class="breadcrumb-item">
+                            <a href="home.html">
+                                <i class="fas fa-home icn"><span class="sr-only">icon</span></i>
+                                Home
+                            </a>
+                        </li>
+                        <li class="breadcrumb-item active" aria-current="page">{{Route::currentRouteName()}}</li>
+                    </ol>
+                </div>
+                <div class="col-12 col-md-5 d-none d-md-flex align-items-md-center justify-content-md-end">
+                    <!-- title -->
+                    <strong class="title d-block text-right fontRoboto fwMedium text-capitalize text-dark">Achievement &amp; Milestones</strong>
+                </div>
+            </div>
+        </div>
+    </nav>
+    <!-- contactAreaBlock -->
+
+
+        <!-- contactAreaBlock -->
+        <section class="contactAreaBlock contentBlock">
+
+            <div class="container" style="text-align:center;padding:40px">
+                <video width="320" height="240" controls style="width:100%;height:auto">
+                    <source src="{{ asset('videos/dnd.mp4') }}" type="video/mp4">
+                    Your browser does not support the video playback.
+                </video>    
+            </div>
+        </section>
+</main>
 @endsection
