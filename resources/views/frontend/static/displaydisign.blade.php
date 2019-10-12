6137d61ddc4b9e1e27511473da89f31f267f9d78
Shashwat Singh <parihar.shashwat@gmail.com>
2019-10-12 12:12:37 +0530

    added achievements page

--- resources/views/frontend/static/displaydisign.blade.php
+++ resources/views/frontend/static/displaydisign.blade.php
@@ -1,7 +1,10 @@
 @extends('layouts.frontend.main')
 
 @section('content')
-    <div class="container" style="text-align:center;padding:40px">
-        <h2>Will Be Updated Soon</h2>
+<div class="container" style="text-align:center;padding:40px">
+    <video width="320" height="240" controls style="width:100%;height:auto">
+        <source src="{{ asset('videos/DND.mp4') }}" type="video/mp4">
+        Your browser does not support the video playback.
+    </video>    
     </div>
 @endsection
