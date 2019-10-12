b5efae454dc4be5f5562f12b3309805114ef23bb
Shashwat Singh <parihar.shashwat@gmail.com>
2019-10-12 12:14:19 +0530

    added achievements page

--- resources/views/frontend/static/displaydisign.blade.php
+++ resources/views/frontend/static/displaydisign.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="container" style="text-align:center;padding:40px">
     <video width="320" height="240" controls style="width:100%;height:auto">
-        <source src="{{ asset('videos/DND.mp4') }}" type="video/mp4">
+        <source src="{{ asset('videos/dnd.mp4') }}" type="video/mp4">
         Your browser does not support the video playback.
     </video>    
     </div>
