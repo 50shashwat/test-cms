32fdb34700a2b6c73a2d12012039318f3bbc8196
Shashwat Singh <parihar.shashwat@gmail.com>
2019-10-12 12:54:15 +0530

    added achievements page

--- resources/views/frontend/static/achievements.blade.php
+++ resources/views/frontend/static/achievements.blade.php
@@ -1,38 +1,71 @@
 @extends('layouts.frontend.main')
 
 @section('content')
-    <div class="container" style="text-align:center;padding:40px">
-        <video width="320" height="240" controls style="width:100%;height:auto">
-            <source src="{{ asset('videos/award.mp4') }}" type="video/mp4">
-            Your browser does not support the video playback.
-        </video>     
+<main>
 
-        <div class="row">
-            <div class="col-md-6">
-                <img src="{{asset('achievements/0001.jpg') }}" class="img img-responsive"/>
-            </div>
-            <div class="col-md-6">
-                <img src="{{asset('achievements/0002.jpg') }}" class="img img-responsive"/> 
+    <!-- pageBdWrapNav -->
+    <nav class="pageBdWrapNav bg-light" aria-label="breadcrumb">
+        <div class="container">
+            <div class="row align-items-md-center">
+                <div class="col-12 col-md-7">
+                    <!-- breadcrumb -->
+                    <ol class="breadcrumb pageBreadcrumb m-0 p-0 text-capitalize">
+                        <li class="breadcrumb-item">
+                            <a href="home.html">
+                                <i class="fas fa-home icn"><span class="sr-only">icon</span></i>
+                                Home
+                            </a>
+                        </li>
+                        <li class="breadcrumb-item active" aria-current="page">{{Route::currentRouteName()}}</li>
+                    </ol>
+                </div>
+                <div class="col-12 col-md-5 d-none d-md-flex align-items-md-center justify-content-md-end">
+                    <!-- title -->
+                    <strong class="title d-block text-right fontRoboto fwMedium text-capitalize text-dark">Milestone &amp; Achievements</strong>
+                </div>
             </div>
         </div>
-        
-        <div class="row">
-            <div class="col-md-6">
-                <img src="{{asset('achievements/0003.jpg') }}" class="img img-responsive"/>
-            </div>
-            <div class="col-md-6">
-                <img src="{{asset('achievements/0004.jpg') }}" class="img img-responsive"/> 
-            </div>
-        </div>
+    </nav>
+    <!-- contactAreaBlock -->
 
-        
-        <div class="row">
-            <div class="col-md-6">
-                <img src="{{asset('achievements/0005.jpg') }}" class="img img-responsive"/>
+
+        <!-- contactAreaBlock -->
+        <section class="contactAreaBlock contentBlock">
+
+            <div class="container" style="text-align:center;padding:40px">
+                <video width="320" height="240" controls style="width:100%;height:auto">
+                    <source src="{{ asset('videos/award.mp4') }}" type="video/mp4">
+                    Your browser does not support the video playback.
+                </video>     
+
+                <div class="row">
+                    <div class="col-md-6">
+                        <img src="{{asset('achievements/0001.jpg') }}" class="img img-responsive"/>
+                    </div>
+                    <div class="col-md-6">
+                        <img src="{{asset('achievements/0002.jpg') }}" class="img img-responsive"/> 
+                    </div>
+                </div>
+                
+                <div class="row">
+                    <div class="col-md-6">
+                        <img src="{{asset('achievements/0003.jpg') }}" class="img img-responsive"/>
+                    </div>
+                    <div class="col-md-6">
+                        <img src="{{asset('achievements/0004.jpg') }}" class="img img-responsive"/> 
+                    </div>
+                </div>
+
+                
+                <div class="row">
+                    <div class="col-md-6">
+                        <img src="{{asset('achievements/0005.jpg') }}" class="img img-responsive"/>
+                    </div>
+                    <div class="col-md-6">
+                        <img src="{{asset('achievements/0006.jpg') }}" class="img img-responsive"/> 
+                    </div>
+                </div>
             </div>
-            <div class="col-md-6">
-                <img src="{{asset('achievements/0006.jpg') }}" class="img img-responsive"/> 
-            </div>
-        </div>
-    </div>
+        </section>
+</main>
 @endsection
