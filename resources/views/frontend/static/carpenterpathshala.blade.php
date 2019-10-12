548c06bb270961bc35a6ab0d8441d80dd5f3b30a
Shashwat Singh <parihar.shashwat@gmail.com>
2019-10-12 12:57:45 +0530

    fixed video height

--- resources/views/frontend/static/carpenterpathshala.blade.php
+++ resources/views/frontend/static/carpenterpathshala.blade.php
@@ -38,7 +38,7 @@
         </div>
         
         <div class="col-md-6" style="text-align:center">
-        <video width="320" height="240" controls>
+        <video width="320" height="240" controls style="width:100%;height:auto">
             <source src="/downloads/palwal.mp4" type="video/mp4">
             Your browser does not support the video playback.
         </video>  
