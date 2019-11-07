fee7c4d5c6a793050836ed3b694bc3f8df851717
Shashwat Singh <parihar.shashwat@gmail.com>
2019-11-07 10:47:01 +0530

    added google analytics

--- resources/views/layouts/frontend/main.blade.php
+++ resources/views/layouts/frontend/main.blade.php
@@ -101,5 +101,16 @@
 	<div id="fb-root"></div>
 	<script async defer crossorigin="anonymous" src="https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v4.0&appId=650492478678159&autoLogAppEvents=1"></script>
 	@yield('script')
+
+	<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-151730631-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-151730631-1');
+</script>
+
 </body>
 </html>
